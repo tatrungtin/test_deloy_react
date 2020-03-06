5a9b6199f989899bbf819de3c1c625216085baa9
Tin Ta <tin.ta@hellohealthgroup.com>
2020-03-06 16:32:30 +0700

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit jenskin
+          Edit jenskin edit 
         </p>
         <a
           className="App-link"
